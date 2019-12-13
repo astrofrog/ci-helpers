53a32dbf513f629e6ac2744251921816a3206ac7
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-12-13 13:27:45 -0700

    Patch tox args

--- tox/apply_ci_patches.sh
+++ tox/apply_ci_patches.sh
@@ -23,8 +23,13 @@ echo "                      @
          #####   .######%          "
 echo ""
 echo "TOXPOSARGS: "$TOXPOSARGS
+echo "TOXARGS: "$TOXARGS
 
 pip install tox-pypi-filter
 export TOXPOSARGS=$TOXPOSARGS" --force-dep='pytest<5'"
 
+TOXARGS="-vvv"
+
 echo "TOXPOSARGS[patched]: "$TOXPOSARGS
+echo "TOXARGS[patched]: "$TOXARGS
+
